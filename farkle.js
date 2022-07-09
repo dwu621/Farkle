b639e9f1cf2b0c494f42622975e8cbe519e8e67a
David Wu <dwu0508@gmail.com>
2022-07-09 13:43:33 -0500

    first commit

---
+++ farkle.js
@@ -0,0 +1,44 @@
+var diceArr = [];
+
+function initializeDice(){
+	for(i = 0; i < 6; i++){
+		diceArr[i] = {};
+		diceArr[i].id = "die" + (i + 1); 
+		diceArr[i].value = i + 1;
+		diceArr[i].clicked = 0;
+	}
+}
+
+/*Rolling dice values*/
+function rollDice(){
+	console.log("clicked")
+	console.log(diceArr)
+	for(var i=0; i < 6; i++){
+		if(diceArr[i].clicked === 0){
+			diceArr[i].value = Math.floor((Math.random() * 6) + 1);
+		}
+	}
+	updateDiceImg();
+}
+
+/*Updating images of dice given values of rollDice*/
+function updateDiceImg(){
+	var diceImage;
+	for(var i = 0; i < 6; i++){
+		diceImage = "images/" + (diceArr[i].value) + ".png";
+		document.getElementById(diceArr[i].id).setAttribute("src", diceImage);
+	}
+}
+
+function diceClick(img){
+	var i = img.getAttribute("data-number");
+
+	img.classList.toggle("transparent");
+	if(diceArr[i].clicked === 0){
+		diceArr[i].clicked == 1;
+	}
+	else{
+		diceArr[i].clicked == 0;
+	}
+
+}
